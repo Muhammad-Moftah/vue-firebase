075cb10c2488a56a120d5ebbae43134f3344cfac
moftah <mohamed.moftah@minvotech.com>
2019-12-05 17:52:37 +0200

    routering Nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="">
+      <ul>
+          <li><rounter-link to="/">Home</rounter-link></li>
+          <li><rounter-link to="/">Mof</rounter-link></li>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
